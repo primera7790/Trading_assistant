2f5cadd2daba81445f7226b7e0e0f458e22dd679
primera7790 <def.primera@gmail.com>
2022-12-12 13:47:07 +0500

    The secret key was hidden

--- trading_assistant/settings.py
+++ trading_assistant/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -22,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-raaixg1_na3(e^pdoo=3*9x1&+pr96h2)9lyz3j9jw0#)zc@fo'
+load_dotenv('program/tech_zone/env/.env')
+SECRET_KEY = os.environ.get('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
