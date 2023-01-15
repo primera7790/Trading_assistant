3c78187842ed997fbea42eb1642480b7ee0f3b63
_primera <def.primera@gmail.com>
2023-01-15 13:54:51 +0500

    soft update
    not usable import commented

--- program/main.py
+++ program/main.py
@@ -10,7 +10,7 @@ from tkinter import messagebox as mb
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
